5d0686e8d62d86aa87118d35b8d7e17c3923e1b6
lidong <lid@qipengkeji.com>
2016-08-16 11:53:56 +0800

    react demo

---
+++ ItemBox/ItemBox.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Item from '../css/ItemBox.css';
+
+class ItemBox extends React.Component {
+    render() {
+        return (
+            <div>
+                <OpenDate/>
+                <RateStart/>
+                <ProjectDetail/>
+            </div>
+        );
+    }
+}
+var OpenDate=React.createClass({
+    getInitialState:function(){
+      return {
+          _openDate:''
+      }
+    },
+    componentDidMount: function() {
+        $.get('./secum_response.json', function(data) {
+            var d=data;
+            this.setState({
+                _openDate:d.info.openDate
+            });
+        }.bind(this));
+    },
+
+    render:function(){
+        var openDate=this.state._openDate;
+        return(
+            <div className={Item.item}>
+                <p>开放日期 <span className={Item.floatR}>{openDate}</span></p>
+                {this.state.username}
+            </div>
+        )
+    }
+});
+
+var RateStart=React.createClass({
+    render:function(){
+        return(
+            <div className={Item.item}>
+                <p>起息日期 <span className={Item.floatR}>购买成功后下一交易日</span></p>
+            </div>
+        )
+    }
+});
+var ProjectDetail=React.createClass({
+    render:function(){
+        return(
+            <div className={Item.item}>
+                <p>项目详情 <span className={Item.floatR}> ></span></p>
+            </div>
+        )
+    }
+});
+
+export default ItemBox;
