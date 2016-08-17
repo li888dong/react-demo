32c9cc6fe55a3db70ce7660ea07cfc38b09104bf
lidong <lid@qipengkeji.com>
2016-08-17 09:21:01 +0800

    react demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+var webpack = require('webpack');
 var config = {
    entry: './main.js',
 	
    output: {
       path:'./',
-      filename: 'index.js',
+      filename: 'index.js'
    },
 	
    devServer: {
@@ -27,8 +28,24 @@ var config = {
             loader: "style-loader!css-loader?modules"
          }
       ]
-   }
+   },
+   plugins:[
+     //署名
+    new webpack.BannerPlugin('This file is created by lidong'),
+    //jquery使用须前安装，npm install jquery
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+      }),
+    //代码压缩
+    //new webpack.optimize.UglifyJsPlugin({
+    //  compress: {
+    //    warnings: false
+    //  }
+    //})
+   ]
 	
-}
+};
 
 module.exports = config;
